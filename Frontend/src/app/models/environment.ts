9381d7f36554bcee77a49303b72803034b89d89c
mehdi220320 <111698017+mehdi220320@users.noreply.github.com>
2025-09-16 10:14:17 +0100

    tests

--- Frontend/src/app/models/environment.ts
+++ Frontend/src/app/models/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   client_id: '443091369200-kq79h2r8bsli9vmneo6vtlsa613g75av.apps.googleusercontent.com',
-  apiUrl:'http://localhost:3000/'
+  apiUrl:'https://elearning-2-mlz0.onrender.com'
 };
