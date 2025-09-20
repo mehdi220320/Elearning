f7b3d46ed0674801759db0f234626f30d0081397
Mohamed Mehdi Ben Gamra <111698017+mehdi220320@users.noreply.github.com>
2025-09-20 10:38:23 +0100

    Update environment.ts

--- Frontend/src/app/models/environment.ts
+++ Frontend/src/app/models/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  client_id: '443091369200-kq79h2r8bsli9vmneo6vtlsa613g75av.apps.googleusercontent.com',
+  client_id: '',
   apiUrl:'https://elearning-2-mlz0.onrender.com/'
 };
